13234ac3a2bd2f7721541a22c91b9efec58089bf
Franco Seguel <francoseguellucero@gmail.com>
2022-11-08 16:58:23 -0300

    Agregados tests para setear y obtener numeros del arreglo para calcular la mediana

--- utils/include/utils.h
+++ utils/include/utils.h
@@ -3,20 +3,151 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// struct mediana
+/**
+ * Estructura Array
+ * 
+ * Representa un array con su largo
+ * 
+ * @param len: Largo del array
+ * @param arr: El arreglo
+*/
+typedef struct array{
+    int len;
+    int *arr;
+}Array;
+
+/**
+ * Estructura Mediana
+ * 
+ * Representa una estructura para el problema de la mediana
+ * 
+ * @param med: La mediana del arreglo
+ * @param a: El Array
+*/
 typedef struct mediana{
     int med;
-    int *arr;
+    Array *arr;
 }Mediana;
 
 //Tiempo del algoritmo
 typedef double (*tiempo_alg)();
 
 //constructores y destructores
+
+/**
+ * newMedian 
+ * 
+ * Entrega una nueva estructura del problema
+ * 
+ * @param array_size: El tamaño del arreglo
+*/
 Mediana *newMedian(int array_size);
+
+/**
+ * Destruye una estructura del problema
+ * 
+ * @param med: La estructura mediana
+*/
 void destroyMedian(Mediana *med);
 
-// test
+/**
+ * newArray
+ * 
+ * Crea un array de largo len
+ * 
+ * @param len: El tamaño del arreglo
+ * 
+*/
+Array *newArray(int len);
+
+/**
+ * destroyArray
+ * 
+ * Destruye la estructura Array
+ * 
+ * @param arr: La estructura Array
+*/
+void destroyArray(Array *arr);
+
+/**
+ * getFromArray 
+ * 
+ * Obtiene el index-ésimo elemento del array
+ * 
+ * @param a: El Array
+ * @param index: El índice
+*/
+int getFromArray(Array *a, int index);
+
+/**
+ * setToArray
+ * 
+ * Cambia el valor del arreglo indicado por index 
+ * a el valor newValue
+ * 
+ * @param a: El Array
+ * @param index: El índice
+ * @param newValue: El nuevo valor
+*/
+void setToArray(Array *a, int index, int newValue);
+
+/**
+ * getArraySize
+ * 
+ * Obtiene el tamaño del arreglo Arr
+ * 
+ * @param a: El Arreglo
+*/
+int getArraySize(Array *a);
+
+/**
+ * getMedianSize
+ * 
+ * Retorna el tamaño del arreglo correspondiente
+ * a la estructura Mediana
+*/
+int getMedianSize(Mediana *med);
+
+/**
+ * getFromMedian
+ * 
+ * Obtiene el index-ésimo elemento del arreglo 
+ * correspondiente a la estructura Mediana
+ * 
+ * @param med: La estructura Mediana
+ * @param index: El índice a consultar
+*/
+int getFromMedian(Mediana *med, int index);
+
+/**
+ * setToMedian
+ * 
+ * Cambia o establece el valor indicado por index en el 
+ * arreglo a newValue de la estructura Mediana
+ * 
+ * @param med: La estructura Mediana
+ * @param index: El índice del arreglo que se quiere cambiar
+ * @param newValue: El nuevo valor al que se quiere actualizar
+*/
+void setToMedian(Mediana *med, int index, int newValue);
+
+
+/**
+ * Test que prueba el correcto funcionamiento de median
+*/
+void testSetGetToMedian(void);
+
+/**
+ * test
+ * 
+ * funcion para probar funcionalidades
+ * 
+ * @param name: Nombre del test
+ * @param expected: Lo que se espera
+ * @param got: Lo que se obtuvo
+*/
 void test(char *name, int expected, int got);
+
+// Función dummy
 void holi();
 
--- utils/src/utils.c
+++ utils/src/utils.c
@@ -1,25 +1,84 @@
 #include <utils.h>
 
-Mediana *newMedian(int array_size){
-    int *a = (int *) calloc(array_size, sizeof(Mediana)); 
-    if( a == NULL){
-        perror("array_mediana");
+Array *newArray(int len){
+    int *arr = calloc(len, sizeof(int));
+    if(arr == NULL){
+        perror("arary not created");
         exit(-1);
     }
+    Array *a = malloc(sizeof(Array));
+    if(a == NULL){
+        perror("Array struct not created");
+    }
+    a->arr = arr;
+    a->len = len;
+
+    return a;
+}
+
+void destroyArray(Array *arr){
+    free(arr->arr);
+    free(arr);
+}
+
+Mediana *newMedian(int array_size){
+    Array *arr = newArray(array_size);
     Mediana *med = (Mediana*) malloc(sizeof(Mediana));
     if (med == NULL){
         perror("mediana struct");
-        free(a);
+        destroyArray(arr);
         exit(-1);
     }
-    med -> arr = a;
+    med -> arr = arr;
     med ->med = 0;
 
     return med;
 }
+
 void destroyMedian(Mediana *med){
-    free(med->arr);
+    destroyArray(med->arr);
     free(med);
+}
+
+int getFromArray(Array *a, int index){
+    return (a->arr)[index];
+}
+
+void setToArray(Array *a, int index, int newValue){
+    a->arr[index] = newValue;
+}
+
+int getArraySize(Array *a){
+    return a->len;
+}
+
+int getMedianSize(Mediana *med){
+    return getArraySize(med->arr);
+}
+
+int getFromMedian(Mediana *med, int index){
+    return getFromArray(med->arr, index);
+}
+
+void setToMedian(Mediana *med, int index, int newValue){
+    setToArray(med->arr, index, newValue);
+}
+
+void testSetGetToMedian(void){
+    int array_size = 10;
+    int a[] = {1,2,3,4,5,6,7,8,9,10};
+
+    Mediana *med = newMedian(array_size);
+    for(int i = 0; i < array_size; i++){
+        setToMedian(med, i, a[i]);
+    }
+
+    for(int i = 0; i < array_size; i++){
+        test("set get to median ", a[i], getFromMedian(med,i));
+    }
+    printf("\n");
+
+    destroyMedian(med);
 }
 
 void test(char *name, int expected, int got){
