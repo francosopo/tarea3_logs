9051aedde194b35ceabdfc8006523407899a9d96
Franco Seguel <francoseguellucero@gmail.com>
2022-11-08 16:57:39 -0300

    lesser elements agregado, falta probar

--- algoritmo_uno/include/alg1.h
+++ algoritmo_uno/include/alg1.h
@@ -2,4 +2,68 @@
 /// En utils.h se define lo que se usará en todos los algoritmos.
 #include <utils.h>
 
+
+
+/**
+ * find_median_alg
+ * 
+ * Encuentra la mediana del alg 1
+ * 
+ * @param m Estructura mediana
+*/
 int find_median_alg1(Mediana *m);
+
+/**
+ * lesser_elements
+ * 
+ * Parte la @param med con respecto a un pivot indicado
+ * en el @param index_pivot, entregando un arreglo de int's 
+ * alocado en el heap con los elementos menores que el pivot
+ * 
+ * @param med: Estructura mediana
+ * @param index_pivot: El índice del pivot
+*/
+int *lesser_elements(Mediana *med, int index_pivot);
+
+/**
+ * greater_elements
+ * 
+ * Parte la @param med con respecto a un pivot indicado 
+ * en el @param index_pivot, entregando un arreglo de int's 
+ * alocado en el heap con los elementos mayores que el pivot
+ * 
+ * @param med: Estructura mediana
+ * @param index_pivot: El índice del pivot
+*/
+int *greater_elements(Mediana *med,int index_pivot);
+
+/**
+ * cut_b
+ * 
+ * Corta el @param arr de tamano m, en m/c pequeños grupos
+ * de tamaño c 
+ * 
+ * @param arr: El arreglo a cortar
+ * @param c: El número c
+*/
+int **cut_b(int *arr, int c);
+
+/**
+ * sort
+ * 
+ * Ordena un arreglo @param arr
+ * 
+ * @param arr: El arreglo a ordenar
+*/
+int *sort(int *arr);
+
+/**
+ * copy_medians
+ * 
+ * Copia las medianas obtenidas de @param arr en
+ * un nuevo arreglo, retornándolo
+ * 
+ * @param arr: El arreglo de medianas
+ * @param size: El tamaño del arreglo de medianas
+*/
+int *copy_medians(int *arr, int size);
--- algoritmo_uno/src/alg1.c
+++ algoritmo_uno/src/alg1.c
@@ -4,10 +4,33 @@ int find_median_alg1(Mediana *med){
     return 1;
 }
 
+int *lesser_elements(Mediana *med, int index_pivot){
+    int *ret_prev = calloc(getMedianSize(med), sizeof(int));
+    int j_ret = 0;
+    for(int i = 0; i < getMedianSize(med); i++){
+        if(getFromMedian(med, i) < getFromMedian(med, index_pivot)){
+            ret_prev[j_ret] = getFromMedian(med, i);
+            j_ret++;
+        }
+    }
+    //copiar el arreglo hasta el tamaño indicado por size
+    int size = j_ret + 1;
+    int *ret = calloc(size, sizeof(int));
+    for(int i = 0; i < size; i++){
+        ret[i] = ret_prev[i];
+    }
+    //librar el arreglo provisorio
+    free(ret_prev);
+
+    // retornar el arreglo limpio
+    return ret;
+}
+
 int main(int argc, char *argv[]){
     int medianSize = 30;
     Mediana *med = newMedian(medianSize);
-    int median = find_median_alg1(med);
-    test("dummy_test", 1, median);
+
+    testSetGetToMedian();
+
     destroyMedian(med);
 }
